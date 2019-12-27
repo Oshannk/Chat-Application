45628a174b73c680c23e51a3c9092e738683136d
Oshannk <oshanmuththettupita@outlook.com>
2019-12-27 17:47:54 +0530

    ChatApp

---
+++ ChatApp/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace ChatApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtsip = new System.Windows.Forms.TextBox();
+            this.txtsport = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnconnect = new System.Windows.Forms.Button();
+            this.txtcport = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtcip = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chatscreen = new System.Windows.Forms.TextBox();
+            this.txtmsg = new System.Windows.Forms.TextBox();
+            this.btnsend = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnstart);
+            this.groupBox1.Controls.Add(this.txtsport);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtsip);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(541, 97);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // txtsip
+            // 
+            this.txtsip.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsip.Location = new System.Drawing.Point(36, 39);
+            this.txtsip.Name = "txtsip";
+            this.txtsip.Size = new System.Drawing.Size(136, 22);
+            this.txtsip.TabIndex = 1;
+            // 
+            // txtsport
+            // 
+            this.txtsport.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsport.Location = new System.Drawing.Point(256, 37);
+            this.txtsport.Name = "txtsport";
+            this.txtsport.Size = new System.Drawing.Size(136, 22);
+            this.txtsport.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(205, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port ";
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(442, 39);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(75, 23);
+            this.btnstart.TabIndex = 4;
+            this.btnstart.Text = "Start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnconnect);
+            this.groupBox2.Controls.Add(this.txtcport);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtcip);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 134);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(541, 97);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Client";
+            // 
+            // btnconnect
+            // 
+            this.btnconnect.Location = new System.Drawing.Point(442, 39);
+            this.btnconnect.Name = "btnconnect";
+            this.btnconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnconnect.TabIndex = 4;
+            this.btnconnect.Text = "Connect";
+            this.btnconnect.UseVisualStyleBackColor = true;
+            this.btnconnect.Click += new System.EventHandler(this.btnconnect_Click);
+            // 
+            // txtcport
+            // 
+            this.txtcport.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcport.Location = new System.Drawing.Point(256, 37);
+            this.txtcport.Name = "txtcport";
+            this.txtcport.Size = new System.Drawing.Size(136, 22);
+            this.txtcport.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(205, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Port ";
+            // 
+            // txtcip
+            // 
+            this.txtcip.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcip.Location = new System.Drawing.Point(36, 39);
+            this.txtcip.Name = "txtcip";
+            this.txtcip.Size = new System.Drawing.Size(136, 22);
+            this.txtcip.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "IP";
+            // 
+            // chatscreen
+            // 
+            this.chatscreen.Location = new System.Drawing.Point(12, 237);
+            this.chatscreen.Multiline = true;
+            this.chatscreen.Name = "chatscreen";
+            this.chatscreen.Size = new System.Drawing.Size(541, 227);
+            this.chatscreen.TabIndex = 6;
+            // 
+            // txtmsg
+            // 
+            this.txtmsg.Location = new System.Drawing.Point(12, 481);
+            this.txtmsg.Multiline = true;
+            this.txtmsg.Name = "txtmsg";
+            this.txtmsg.Size = new System.Drawing.Size(446, 33);
+            this.txtmsg.TabIndex = 7;
+            // 
+            // btnsend
+            // 
+            this.btnsend.Location = new System.Drawing.Point(464, 483);
+            this.btnsend.Name = "btnsend";
+            this.btnsend.Size = new System.Drawing.Size(89, 28);
+            this.btnsend.TabIndex = 5;
+            this.btnsend.Text = "Send";
+            this.btnsend.UseVisualStyleBackColor = true;
+            this.btnsend.Click += new System.EventHandler(this.btnsend_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 539);
+            this.Controls.Add(this.btnsend);
+            this.Controls.Add(this.txtmsg);
+            this.Controls.Add(this.chatscreen);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Chat Application";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.TextBox txtsport;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtsip;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnconnect;
+        private System.Windows.Forms.TextBox txtcport;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtcip;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox chatscreen;
+        private System.Windows.Forms.TextBox txtmsg;
+        private System.Windows.Forms.Button btnsend;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ ChatApp/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace ChatApp
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string recieve;
+        public string TextToSend;
+
+        public Form1()
+        {
+            InitializeComponent();
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach(IPAddress address in localIP)
+            {
+                if(address.AddressFamily == AddressFamily.InterNetwork){
+                    txtsip.Text = address.ToString();
+                }
+            }
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(txtsport.Text));
+            listener.Start();
+            client = listener.AcceptTcpClient();
+            STR = new StreamReader(client.GetStream());
+            STW = new StreamWriter(client.GetStream());
+            STW.AutoFlush = true;
+            backgroundWorker1.RunWorkerAsync();
+            backgroundWorker2.WorkerSupportsCancellation = true;
+        }
+
+        private void btnconnect_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient();
+            IPEndPoint ipend = new IPEndPoint(IPAddress.Parse(txtcip.Text), int.Parse(txtcport.Text));
+            try
+            {
+                client.Connect(ipend);
+                if (client.Connected)
+                {
+                    chatscreen.AppendText("Connected To Server" + "\n");
+                    STR = new StreamReader(client.GetStream());
+                    STW = new StreamWriter(client.GetStream());
+                    STW.AutoFlush = true;
+                    backgroundWorker1.RunWorkerAsync();
+                    backgroundWorker2.WorkerSupportsCancellation = true;
+                }
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    recieve = STR.ReadLine();
+                    this.chatscreen.Invoke(new MethodInvoker(delegate ()
+                    {
+                        chatscreen.AppendText("\nClient:" + recieve + "\n");
+                    }));
+                    recieve = "";
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (client.Connected)
+            {
+                STW.WriteLine(TextToSend);
+                this.chatscreen.Invoke(new MethodInvoker(delegate ()
+                {
+                    chatscreen.AppendText("You" + TextToSend + " \n");
+                }));
+            }
+            else
+            {
+                MessageBox.Show("Sending Failed");
+            }
+            backgroundWorker2.CancelAsync();
+        }
+
+        private void btnsend_Click(object sender, EventArgs e)
+        {
+            if(txtmsg.Text != "")
+            {
+                TextToSend = txtmsg.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            txtmsg.Text = "";
+        }
+    }
+}
